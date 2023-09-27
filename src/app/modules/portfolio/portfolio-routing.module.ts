93c7ca6883409375eb258e28c8a6b05077b6faf4
tasheela <tasheelajay1999@gmail.com>
2023-09-27 19:29:10 +0530

    not found page added and routing completed

--- src/app/modules/portfolio/portfolio-routing.module.ts
+++ src/app/modules/portfolio/portfolio-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio.component';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {HobbiesComponent} from "./components/hobbies/hobbies.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
-const routes: Routes = [{ path: '', component: PortfolioComponent }];
+const routes: Routes = [
+  { path: '', component: PortfolioComponent , children:[
+      {path:'',redirectTo:'landing',pathMatch:'full'},
+      {path:'landing',component:LandingPageComponent},
+      {path:'home',component:HomeComponent},
+      {path:'about',component:AboutComponent},
+      {path:'projects',component:ProjectsComponent},
+      {path:'hobbies',component:HobbiesComponent},
+      {path:'contact',component:ContactComponent}
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
